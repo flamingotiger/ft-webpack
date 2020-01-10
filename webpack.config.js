d2b8f62d597fa45d09ff019a4b466e0c0747225c
hakbum <hhbhong@gmail.com>
2020-01-11 02:09:46 +0900

    More optimization webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,25 @@ const webpackDevServer = __DEV__ ? {
   }
 } : {};
 
+const splitChunk = !__DEV__ ? {} : {
+  splitChunks: {
+    chunks: 'async',
+    minSize: 30000,
+    minRemainingSize: 0,
+    maxSize: 0,
+    minChunks: 1,
+    maxAsyncRequests: 6,
+    maxInitialRequests: 4,
+    automaticNameDelimiter: '~',
+    automaticNameMaxLength: 30,
+  }
+}
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: __DEV__ ? '[name].js' : '[name].[chunkhash].js',
-    path: path.resolve(__dirname + '/build')
+    path: path.resolve(__dirname + 'build')
   },
   ...webpackDevServer,
   mode: __DEV__ ? "development" : "production",
@@ -24,6 +38,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
         use: ["babel-loader", "stylelint-custom-processor-loader"]
       },
       {
@@ -43,5 +58,8 @@ module.exports = {
       filename: 'index.html'
     }),
     new CleanWebpackPlugin()
-  ]
+  ],
+  optimization: {
+    ...splitChunk
+  }
 }
