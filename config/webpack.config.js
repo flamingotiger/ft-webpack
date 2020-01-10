ffd9fbd3f3c9611847a50653f28fa7eeffb797fe
flamingotiger <hhbhong@gmail.com>
2020-01-10 18:23:27 +0900

    Add webpack config but error

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,11 +1,37 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: 'index.js',
+  entry: '../src/index.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js'
+    path: path.resolve(__dirname, '/dist'),
+    filename: '[name].[hash].bundle.js',
   },
-  plugins:[new HtmlWebpackPlugin()]
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: '../src/index.html',
+    filename: './index.html'
+  }), new webpack.ProgressPlugin()],
 };
